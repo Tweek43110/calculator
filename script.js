2e22679e0857021b043d2c0d1b01fc3b7e3f4dcf
Alex Coe <alex.r.coe@gmail.com>
2019-01-13 15:13:17 -0600

    Added math module and operate function

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 
-function add(addNum, addNum2) {
-    return addNum + addNum2;
+function operate(operator, num1, num2){
+    if (operator === '+') {
+        return math.add(num1, num2);
+    } else if (operator === '-') {
+        return math.subtract(num1, num2);
+    } else if (operator === '*') {
+        return math.multiply(num1, num2);
+    } else if (operator === '/') {
+        return math.divide(num1, num2);
+    } else {
+        alert('Something has gone wrong!');
+    }
 }
 
-function subtract(subNum, subNum2) {
-    return subNum - subNum2;
-}
+let math = {
+    add: function add(addNum, addNum2) {
+        return addNum + addNum2;
+    },
 
-function multiply(multNum, multNum2) {
-    return multNum * multNum2;
-}
+    subtract: function subtract(subNum, subNum2) {
+        return subNum - subNum2;
+    },
 
-function divide(divNum, divNum2) {
-    return divNum / divNum2;
+    multiply: function multiply(multNum, multNum2) {
+        return multNum * multNum2;
+    },
+
+    divide: function divide(divNum, divNum2) {
+        return divNum / divNum2;
+    }
 }
