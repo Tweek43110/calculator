848a7c5e301944594938d57d3ead52ec2acec0f8
Alex Coe <alex.r.coe@gmail.com>
2019-01-15 09:38:12 -0600

    Equals button event

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+let output = document.getElementById('output');
 let btn = document.getElementsByClassName('btn');
+let globalAnswer = 0;  //To be changed once the rest is sorted out
+
 //Classes throw an HTML collection so event listener must be added via indexer
 for (i=0; i < btn.length; i++){
     btn[i].addEventListener('click', buttonClick);
 }
 
 function buttonClick() {
-    console.log("You clicked " + this.innerHTML);
+    let btnClicked = this.innerHTML;
+    console.log('You clicked ' + btnClicked);
+    output.textContent = btnClicked;
 }
+
+function equals(event) {
+    let equals = document.getElementById('equals');
+    equals.addEventListener('click', function(event) {
+        console.log('Equals button pressed!');
+        let answer = globalAnswer;
+        output.textContent = answer;
+    })
+}
+equals(event)
 
 
 
