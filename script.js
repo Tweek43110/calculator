216db828c3ec04d141d2320a1972c11e55fd88ff
Alex Coe <alex.r.coe@gmail.com>
2019-01-15 14:18:47 -0600

    Attempt to add multidigit

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
+
 let output = document.getElementById('output');
 let storedValues = [];
+
+let math = {
+    add: function add(addNum, addNum2) {
+        return addNum + addNum2;
+    },
+
+    subtract: function subtract(subNum, subNum2) {
+        return subNum - subNum2;
+    },
+
+    multiply: function multiply(multNum, multNum2) {
+        return multNum * multNum2;
+    },
+
+    divide: function divide(divNum, divNum2) {
+        return divNum / divNum2;
+    }
+}
+
+//To call, operator must be string or else there is an error
+function operate(operator, num1, num2){
+    if (operator === '+') {
+        return math.add(num1, num2);
+    } else if (operator === '-') {
+        return math.subtract(num1, num2);
+    } else if (operator === '*') {
+        return math.multiply(num1, num2);
+    } else if (operator === '/') {
+        return math.divide(num1, num2);
+    } else {
+        alert('Something has gone wrong!');
+    }
+}
 
 let btn = document.getElementsByClassName('btn');
 //Classes throw an HTML collection so event listener must be added via indexer
@@ -8,6 +42,40 @@ for (i=0; i < btn.length; i++){
 }
 
 
+//**********************************************************************************************Working on this
+function multipleNumbers() {
+    const lastAdded = storedValues[storedValues.length -1];
+    //test for a follow up digit or float
+    const check = /^-?\d+\.?\d*$/g.test(lastAdded);
+    if (check === true) {
+        //Can't seen to get this to work
+        storedValues.join('');
+         console.log('true');
+    } else {
+        console.log('false')
+     
+    }
+
+}
+
+// ****************************************************************************Combines multiple operands needs bug fixing
+// function checkForTwoOperands(newOperand) {
+//     const lastButtonClicked = storedValues[storedValues.length-1]
+//     const test = /[\+\-\/\*]/g.test(lastButtonClicked);
+
+//     if (test === true) {
+//         storedValues.pop();
+//         storedValues.push(newOperand);
+//         output.setAttribute('value', storedValues.join(''));
+//         console.log('true');
+//     } else {
+//         storedValues.push(newOperand);
+//         output.setAttribute('value', storedValues.join(''));
+//         console.log('false');
+//     }
+// }
+
+//Depends on buttonClick
 function currentValue() {
     let currentValue = document.getElementById('output').innerHTML;
     //exceptions to be left as strings
@@ -24,10 +92,13 @@ function currentValue() {
 function buttonClick() {
     let btnClicked = this.innerHTML;
     output.textContent = btnClicked;
+    multipleNumbers();
     addClick();
+    //checkForTwoOperands();
+
 }
 
-//Pushes clicked buttons to the array
+//Depends on currentValue, pushes clicked buttons to the array
 function addClick() {
     let toAdd = currentValue();
     storedValues.push(toAdd)
@@ -50,42 +121,8 @@ equals(event)
 function clearValues(cleared) {
     let clear = document.getElementById('clear');
     clear.addEventListener('click', function(cleared) {
-        output.textContent = "Cleared";
+        output.textContent = 'Cleared';
         return storedValues = [];
     })
 }
 clearValues()
-
-
-//To call, operator must be string or else there is an error
-function operate(operator, num1, num2){
-    if (operator === "+") {
-        return math.add(num1, num2);
-    } else if (operator === '-') {
-        return math.subtract(num1, num2);
-    } else if (operator === '*') {
-        return math.multiply(num1, num2);
-    } else if (operator === '/') {
-        return math.divide(num1, num2);
-    } else {
-        alert('Something has gone wrong!');
-    }
-}
-
-let math = {
-    add: function add(addNum, addNum2) {
-        return addNum + addNum2;
-    },
-
-    subtract: function subtract(subNum, subNum2) {
-        return subNum - subNum2;
-    },
-
-    multiply: function multiply(multNum, multNum2) {
-        return multNum * multNum2;
-    },
-
-    divide: function divide(divNum, divNum2) {
-        return divNum / divNum2;
-    }
-}
